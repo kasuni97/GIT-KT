fa80ca38fa3fd0b12b8fc3daa60355937ea7406f
Kasuni Kulasekara <kasuni.anu.k@gmail.com>
2026-09-03 21:24:54 +0530

    existing list py change

--- list.py
+++ list.py
@@ -2,6 +2,7 @@ roll_no = [1,2,3,4,5]
 names = ["Kasuni", "Sam","Ann", "Sony", "Danny"]
 mix_list= [1, "ann", True, 1010]
 print(roll_no)
+print(roll_no)
 print(roll_no[0])
 print(roll_no[1])
 print(names[2])
